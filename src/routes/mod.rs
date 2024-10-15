41fb62c476bd661ea2319cf55d8ae8e39225819d
Juliandev02 <julian@strawberryfoundations.org>
2024-10-15 13:19:04 +0200

    feat: add route module

---
+++ src/routes/mod.rs
@@ -0,0 +1,26 @@
+pub mod root;
+pub mod web_static;
+
+pub mod v2 {
+    pub mod index;
+    pub mod login;
+    pub mod login_oauth;
+    pub mod register;
+    pub mod logout;
+    pub mod oauth_dialog;
+    pub mod account;
+
+    pub mod api {
+        pub mod callback;
+        pub mod request;
+        pub mod auth;
+
+        pub mod oauth {
+            pub mod callback;
+        }
+    }
+}
+
+pub mod v1 {
+    pub mod index;
+}
