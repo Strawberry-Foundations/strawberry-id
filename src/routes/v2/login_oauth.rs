49d453fe6e1629a41d99a57a7386b3fc0c151dc9
Juliandev02 <julian@strawberryfoundations.org>
2024-10-15 13:12:49 +0200

    feat: add oauth

---
+++ src/routes/v2/login_oauth.rs
@@ -0,0 +1,139 @@
+use rocket::form::Form;
+use rocket::http::CookieJar;
+use rocket::response::Redirect;
+use rocket::State;
+use rocket_dyn_templates::{context, Template};
+
+use crate::global::{CONFIG, CORE, DATABASE};
+use crate::core::locale::{Language, LANGUAGES};
+use crate::core::object::{OAuthForm, OAuthMeta};
+use crate::core::state::AppState;
+use crate::core::system_core::AnyResponder;
+use crate::utilities::name_parser;
+
+#[get("/login/oauth")]
+pub async fn login_oauth_no_lang(lang: Option<Language>) -> Redirect {
+    match lang {
+        Some(Language::German) => Redirect::to("/v2/de/login"),
+        Some(Language::English) => Redirect::to("/v2/en/login"),
+        _ => Redirect::to("/v2/en/login"), // Default to English if language not specified or supported
+    }
+}
+
+#[get("/<lang>/login/oauth")]
+pub async fn login_oauth_lang_redir(lang: &str) -> Redirect {
+    match lang {
+        "de" => Redirect::to("/v2/de/login"),
+        "en" => Redirect::to("/v2/en/login"),
+        _ => Redirect::to("/v2/en/login"), // Default to English if language not specified or supported
+    }
+}
+
+#[get("/<lang>/login/oauth/<service>", rank = 3)]
+pub async fn login_oauth(lang: &str, state: &State<AppState>, service: &str, jar: &CookieJar<'_>) -> AnyResponder {
+    let mut meta = OAuthMeta {
+        animation: String::from("slide-in-login"),
+        ..Default::default()
+    };
+
+    if !LANGUAGES.contains(&lang) {
+        return AnyResponder::Template(Template::render("404", context! {
+            uri: format!("/v2/{lang}/login")
+        }));
+    };
+
+    meta.service = service.to_string();
+
+    meta.service_name = name_parser(state, &meta.service);
+    meta.trusted = CONFIG.vars.trusted_services.contains(&meta.service);
+
+    let strings = state.messages.get(lang).cloned().unwrap();
+
+    let service_after_login = strings.service_after_login.replace("%s", meta.service_name.as_str());
+
+
+    match jar.get_private("_strawberryid.username") {
+        Some(_) => { },
+        None => return AnyResponder::Redirect(Box::from(Redirect::to(format!("/v2/{lang}/login?service={}&oauth=true", meta.service))))
+    }
+
+    AnyResponder::Template(Template::render("login_oauth", context! {
+        title: &strings.login,
+        strings: &strings,
+        lang: &lang,
+        meta: &meta,
+        service_after_login: &service_after_login,
+    }))
+}
+
+
+#[post("/<lang>/login/oauth/<service>", data = "<form>")]
+pub async fn login_oauth_post(lang: &str, service: &str, form: Form<OAuthForm>, state: &State<AppState>, jar: &CookieJar<'_>) -> AnyResponder {
+    let mut meta = OAuthMeta {
+        animation: String::from("none"),
+        ..Default::default()
+    };
+
+    if !LANGUAGES.contains(&lang) {
+        return AnyResponder::Template(Template::render("404", context! {
+            uri: format!("/v2/{lang}/login")
+        }));
+    };
+
+    meta.service = service.to_string();
+
+    meta.service_name = name_parser(state, &meta.service);
+    meta.trusted = CONFIG.vars.trusted_services.contains(&meta.service);
+
+
+    let code = form.code;
+
+    let strings = state.messages.get(lang).cloned().unwrap();
+    let strings_clone = strings.clone();
+
+    let service_after_login = strings.service_after_login.replace("%s", meta.service_name.as_str());
+
+    if code == 0 || code.to_string().len() != 10 {
+        meta.error = true;
+        meta.info_message = strings.error_invalid_code;
+
+        AnyResponder::Template(Template::render("login_oauth", context! {
+            title: &strings.login,
+            strings: &strings_clone,
+            lang: &lang,
+            meta: &meta,
+            service_after_login: &service_after_login,
+        }))
+    }
+    else {
+        if let Some(value) = CORE.write().await.oauth_codes.get_mut(&code) {
+            if let Some(mut first_value) = value.first().cloned() {
+                let username = match jar.get_private("_strawberryid.username") {
+                    Some(res) => res,
+                    None => return AnyResponder::Redirect(Box::from(Redirect::to(
+                        format!("/v2/{lang}/login?service={}&oauth=true", meta.service)
+                        )))
+                };
+
+                let user_data = DATABASE.get_user_data(username.value().to_string()).await;
+
+                first_value.1 = user_data;
+
+                *value = vec![(username.value().to_string(), first_value.1, first_value.2, first_value.3)];
+            }
+            meta.info_message = strings.code_valid
+
+        } else {
+            meta.error = true;
+            meta.info_message = strings.error_invalid_code;
+        }
+
+        AnyResponder::Template(Template::render("login_oauth", context! {
+            title: &strings.login,
+            strings: &strings_clone,
+            lang: &lang,
+            meta: &meta,
+            service_after_login: &service_after_login,
+        }))
+    }
+}
---
+++ src/routes/v2/oauth_dialog.rs
@@ -0,0 +1,113 @@
+use rocket::http::CookieJar;
+use rocket::response::Redirect;
+use rocket::State;
+use rocket_dyn_templates::{context, Template};
+
+use crate::global::{CONFIG, CORE, DATABASE};
+use crate::core::locale::LANGUAGES;
+use crate::core::object::OAuthMeta;
+use crate::core::params::ApiParams;
+use crate::core::state::AppState;
+use crate::core::system_core::AnyResponder;
+use crate::utilities::name_parser;
+
+#[get("/<lang>/login/oauth_dialog/<service>?<params..>")]
+pub async fn oauth_permit_dialog(lang: &str, service: &str, params: ApiParams, state: &State<AppState>, jar: &CookieJar<'_>) -> AnyResponder {
+    let code = params.code;
+
+    if !LANGUAGES.contains(&lang) {
+        return AnyResponder::Template(Template::render("404", context! {
+            uri: format!("/v2/{lang}/login")
+        }));
+    };
+
+    let mut meta = OAuthMeta {
+        service: service.to_string(),
+        service_name: name_parser(state, service),
+        trusted: CONFIG.vars.trusted_services.contains(&service.to_string()),
+        ..Default::default()
+    };
+
+    match jar.get_private("_strawberryid.username") {
+        Some(res) => res,
+        None => return AnyResponder::Redirect(Box::from(Redirect::to(
+            format!("/v2/{lang}/login?service={}&oauth=true&code={code}", meta.service)
+        )))
+    };
+
+    let strings = state.messages.get(lang).cloned().unwrap();
+
+    if !CORE.read().await.oauth_codes.contains_key(&code) {
+        meta.error = true;
+        meta.info_message = strings.error_invalid_code.clone();
+    }
+
+    let service_after_login = strings.service_after_login.replace("%s", meta.service_name.as_str());
+    let service_wants_to_login = strings.service_wants_to_login
+        .replace("%1", meta.service_name.as_str())
+        .replace("%2", format!("<u>{}</u>", code).as_str());
+
+    AnyResponder::Template(Template::render("oauth_dialog", context! {
+        title: &strings.login,
+        lang: &lang,
+        strings: &strings,
+        meta: &meta,
+        service_after_login: &service_after_login,
+        service_wants_to_login: &service_wants_to_login,
+        code: &code,
+    }))
+}
+
+#[post("/<lang>/login/oauth_dialog/<service>?<params..>")]
+pub async fn oauth_permit_dialog_post(lang: &str, service: &str, params: ApiParams, state: &State<AppState>, jar: &CookieJar<'_>) -> AnyResponder {
+    let code = params.code;
+
+    if !LANGUAGES.contains(&lang) {
+        return AnyResponder::Template(Template::render("404", context! {
+            uri: format!("/v2/{lang}/login")
+        }));
+    };
+
+    let mut meta = OAuthMeta {
+        service: service.to_string(),
+        service_name: name_parser(state, service),
+        trusted: CONFIG.vars.trusted_services.contains(&service.to_string()),
+        ..Default::default()
+    };
+
+    let username = match jar.get_private("_strawberryid.username") {
+        Some(res) => res,
+        None => return AnyResponder::Redirect(Box::from(Redirect::to(
+            format!("/v2/{lang}/login?service={}&oauth=true", meta.service)
+        )))
+    };
+
+    let strings = state.messages.get(lang).cloned().unwrap();
+
+
+    if let Some(value) = CORE.write().await.oauth_codes.get_mut(&code) {
+        if let Some(mut first_value) = value.first().cloned() {
+            let user_data = DATABASE.get_user_data(username.value().to_string()).await;
+
+            first_value.1 = user_data;
+
+            *value = vec![(username.value().to_string(), first_value.1, first_value.2, first_value.3)];
+        }
+        meta.info_message = strings.code_valid.clone()
+
+    } else {
+        meta.error = true;
+        meta.info_message = strings.error_invalid_code.clone();
+    }
+
+    let login_success_subtitle = strings.login_success_subtitle.replace("%s", meta.service_name.as_str());
+
+    AnyResponder::Template(Template::render("oauth_dialog_success", context! {
+        title: &strings.login,
+        lang: &lang,
+        strings: &strings,
+        meta: &meta,
+        code: &code,
+        login_success_subtitle: &login_success_subtitle
+    }))
+}
