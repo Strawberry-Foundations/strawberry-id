12f35fe38f53af9a6683ec2e9262a45f2a9e539c
Juliandev02 <julian@strawberryfoundations.org>
2024-10-15 13:39:23 +0200

    feat: add 404 not found error catcher

---
+++ src/routes/status_code.rs
@@ -0,0 +1,9 @@
+use rocket::Request;
+use rocket_dyn_templates::{context, Template};
+
+#[catch(404)]
+pub async fn not_found(req: &Request<'_>) -> Template {
+    Template::render("404", context! {
+        uri: req.uri()
+    })
+}
