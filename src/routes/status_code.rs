4fabcff273ee11fc64346992b35d9376b80f0d48
Juliandev02 <julian@strawberryfoundations.org>
2024-12-23 20:23:03 +0100

    feat: update 404 page layout and styling with new CSS and improved structure

--- src/routes/status_code.rs
+++ src/routes/status_code.rs
@@ -1,9 +1,12 @@
-use rocket::Request;
+use rocket::{Request, State};
 use rocket_dyn_templates::{context, Template};
+use crate::core::state::AppState;
 
 #[catch(404)]
 pub async fn not_found(req: &Request<'_>) -> Template {
     Template::render("404", context! {
-        uri: req.uri()
+        uri: req.uri(),
+        title: "404 Not Found",
+        lang: "en",
     })
 }
