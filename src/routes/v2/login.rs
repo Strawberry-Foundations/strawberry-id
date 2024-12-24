2514269567f45af7eed533a90683881017584537
Juliandev02 <julian@strawberryfoundations.org>
2024-12-24 01:07:02 +0100

    feat: implement TOTP handling in login process with temporary storage

--- src/routes/v2/login.rs
+++ src/routes/v2/login.rs
@@ -1,33 +1,40 @@
-use std::fmt::Write;
 use rocket::form::Form;
 use rocket::http::CookieJar;
 use rocket::response::Redirect;
 use rocket::State;
 use rocket_dyn_templates::{context, Template};
+use uuid::Uuid;
+use std::collections::HashMap;
+use tokio::sync::RwLock;
+use lazy_static::lazy_static;
 
 use crate::core::locale::{Language, LANGUAGES, Messages};
-use crate::core::object::{CodeType, LoginForm, LoginMeta};
+use crate::core::object::{CodeType, LoginForm, LoginMeta, UserData};
 use crate::core::params::LoginParams;
 use crate::core::state::AppState;
-use crate::core::system_core::{Account, AnyResponder, verify_password};
+use crate::core::system_core::{AnyResponder, verify_password};
 use crate::global::{CORE, DATABASE};
 
+
+lazy_static! {
+    pub static ref TEMP_STORAGE: RwLock<HashMap<String, UserData>> = RwLock::new(HashMap::new());
+}
+
 pub async fn template_responder(
-    strings: Messages, lang: &str, params: LoginParams, meta: LoginMeta,
-    full_params: &String, redirect_after_login: &String, service_after_login: &String
+    strings: &Messages, lang: &str, params: &LoginParams, meta: &LoginMeta,
+    full_params: &str, redirect_after_login: &str, service_after_login: &str
 ) -> AnyResponder {
     AnyResponder::Template(Template::render("login", context! {
         title: &strings.login,
-        strings: &strings,
-        lang: &lang,
-        meta: &meta,
-        params: &params,
-        redirect_after_login: &redirect_after_login,
-        service_after_login: &service_after_login,
-        full_params: &full_params
+        strings: strings,
+        lang: lang,
+        meta: meta,
+        params: params,
+        redirect_after_login: redirect_after_login,
+        service_after_login: service_after_login,
+        full_params: full_params
     }))
 }
-
 
 #[get("/login")]
 pub async fn login_no_lang(lang: Option<Language>) -> Redirect {
@@ -49,8 +56,8 @@ pub async fn login(lang: &str, state: &State<AppState>, mut params: LoginParams,
     let meta = LoginMeta::collect(&mut params, lang, state);
     let strings = state.messages.get(lang).cloned().unwrap();
 
-    let redirect_after_login = strings.redirect_after_login.replace("%s", params.redirect.as_str());
-    let service_after_login = strings.service_after_login.replace("%s", meta.service_name.as_str());
+    let redirect_after_login = strings.redirect_after_login.replace("%s", &params.redirect);
+    let service_after_login = strings.service_after_login.replace("%s", &meta.service_name);
 
     let full_params = match uri.query() {
         Some(query) => query.to_string(),
@@ -81,8 +88,8 @@ pub async fn login_post(
 
     let strings = state.messages.get(lang).cloned().unwrap();
 
-    let redirect_after_login = strings.redirect_after_login.replace("%s", params.redirect.as_str());
-    let service_after_login = strings.service_after_login.replace("%s", meta.service_name.as_str());
+    let redirect_after_login = strings.redirect_after_login.replace("%s", &params.redirect);
+    let service_after_login = strings.service_after_login.replace("%s", &meta.service_name);
 
     let full_params = match uri.query() {
         Some(query) => query.to_string(),
@@ -94,7 +101,7 @@ pub async fn login_post(
         None => {
             meta.info_message = strings.wrong_username.clone();
             meta.error = true;
-            return template_responder(strings, lang, params, meta, &full_params, &redirect_after_login, &service_after_login).await
+            return template_responder(&strings, lang, &params, &meta, &full_params, &redirect_after_login, &service_after_login).await
         }
     };
 
@@ -105,8 +112,25 @@ pub async fn login_post(
         if data.account_enabled == "false" {
             meta.info_message = strings.account_disabled.clone();
             meta.error = true;
-            return template_responder(strings, lang, params, meta, &full_params, &redirect_after_login, &service_after_login).await
+            return template_responder(&strings, lang, &params, &meta, &full_params, &redirect_after_login, &service_after_login).await
         };
+
+        if data.totp_enabled == "true" {
+            meta.totp_required = true;
+            let temp_token = Uuid::new_v4().to_string();
+            jar.add_private(rocket::http::Cookie::new("_strawberryid.temp_token", temp_token.clone()));
+            TEMP_STORAGE.write().await.insert(temp_token, data);
+            return AnyResponder::Template(Template::render("login", context! {
+                title: &strings.login,
+                strings: &strings,
+                lang: lang,
+                meta: meta,
+                params: params,
+                redirect_after_login: redirect_after_login,
+                service_after_login: service_after_login,
+                full_params: full_params
+            }))
+        }
 
         let code = CORE.write().await.generate_code(user_data, CodeType::Website);
 
@@ -154,6 +178,6 @@ pub async fn login_post(
     else {
         meta.info_message = strings.wrong_username_or_password.clone();
         meta.error = true;
-        template_responder(strings, lang, params, meta, &full_params, &redirect_after_login, &service_after_login).await
+        template_responder(&strings, lang, &params, &meta, &full_params, &redirect_after_login, &service_after_login).await
     }
 }
